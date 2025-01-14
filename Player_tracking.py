b4f086348919006a34c5593506f4ffcc96521b79
Ammar-Ali234 <mammarali299@gmail.com>
2025-01-14 18:32:50 +0500

    Updates

--- Player_tracking.py
+++ Player_tracking.py
@@ -1,5 +1,39 @@
 from ultralytics import YOLO
 import cv2
-import numpy as np
 
 model = YOLO("yolo11n.pt")
+video_path = "input_video.mp4"
+cap = cv2.VideoCapture(video_path)
+
+if not cap.isOpened():
+    exit()
+
+output_path = "output_video.mp4"
+fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+fps = int(cap.get(cv2.CAP_PROP_FPS))
+frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+out = cv2.VideoWriter(output_path, fourcc, fps, (frame_width, frame_height))
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    results = model(frame)
+
+    for result in results:
+        for box in result.boxes:
+            if int(box.cls) == 0:
+                x1, y1, x2, y2 = map(int, box.xyxy[0])
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.putText(frame, "Player", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    out.write(frame)
+    cv2.imshow("YOLO Detection", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
